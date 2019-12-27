c4dbbca602923a558568b3656377647eadaf71eb
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2019-12-26 23:42:40 -0300

    Add basic Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CredentialListComponent } from './components/credential-list/credential-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "credentials", component: CredentialListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [CredentialListComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CredentialListComponent } from './components/credential-list/credential-list.component';
 import { CredentialService } from './services/CredentialService';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CredentialListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
